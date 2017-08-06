b3c86393c7af8fd1f89079782ba8a8614ccf7dcc
Darryn Wu <darryn8wu@gmail.com>
2017-08-06 13:02:31 -0400

    fixed multiple photo uploads

--- src/api.js
+++ src/api.js
@@ -2,33 +2,68 @@
 const api = require('express').Router();
 const fs = require('fs');
 
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
+const gallery = require('express-photo-gallery');
+// const multer  = require('multer');
+// const upload = multer({ dest: 'uploads/' });
+
+// const { exec } = require('child_process');
+
+
+const shortid = require('shortid');
+const uploader = require('raw-uploader');
+
 api.use(bodyParser.urlencoded({extended: false}));
 api.use(bodyParser.json());
 
-// If you aren't getting to this object, but rather the index.html (something with a joke) your path is wrong.
-	// I know this because we automatically send index.html for all requests that don't make sense in our backend.
-	// Ideally you would have something to handle this, so if you have time try that out!
+let options = {
+  title: 'Cohort 1706!!!'
+};
+
+api.use('/photos', gallery('./src/img/', options));
+
 
 //Easter Egg - http://localhost:8080/api/hello
 api.get('/hello', (req, res) => res.send({hello: 'world'}))
 
+
 //POST
-api.post('/upload', (req, res, next) => {
-
-    var fstream;
-    req.pipe(req.busboy);
-    req.busboy.on('file', function (fieldmname, file, filename) {
-      console.log("Uploading: " + filename);
-
-      //Path where image will be uploaded
-      fstream = fs.createWriteStream(__dirname + '/img/' + filename);
-      file.pipe(fstream);
-      fstream.on('close', function(){
-        console.log('Upload Finished of ' + filename);
-      });
+api.post('/upload', function (req, res) {
+    let fileName = shortid.generate();
+    uploader({
+        contentTypes: ['image/jpeg'],
+        minFileSize: 100,
+        maxFileSize: 1000000000,
+        outputType: 'file',
+        destFolder: './src/img',
+        createDestFolder: true,
+        disconnectOnErr: true,
+        fileNameHeader: 'file-name',
+        destFileName: fileName,
+        overwrite: true,
+        deepCheckMime: true
+    }, req, res, function (code, data) {
+        res.status(code);
+        res.end();
     });
 });
 
+// api.post('/upload', (req, res, next) => {
+
+//     var fstream;
+//     req.pipe(req.busboy);
+
+//     req.busboy.on('file', function (fieldmname, file, filename) {
+//       console.log("Uploading: " + filename);
+//       //Path where image will be uploaded
+//       fstream = fs.createWriteStream('./src/img/' + counterFileName + filename);
+//       file.pipe(fstream);
+//       fstream.on('close', function(){
+//         console.log('Upload Finished of ' + filename);
+//         counterFileName += "a";
+//       });
+//     });
+
+// });
 
 module.exports = api
